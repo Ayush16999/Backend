e8bca48070168a68adedbffc465a50cf56804615
Ayush16999 <107892183+Ayush16999@users.noreply.github.com>
2022-09-09 22:30:07 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const signupBtn = document.querySelector(".signupBtn");
+const LoginBtn = document.querySelector(".LoginBtn");
+const moveBtn = document.querySelector(".moveBtn");
+const signup = document.querySelector(".signup");
+const login = document.querySelector(".login");
+
+LoginBtn.addEventListener("click",()=>{
+    moveBtn.classList.add("rightBtn");
+    login.classList.add("loginform");
+    signup.classList.remove("signupform");
+    moveBtn.innerHTML = "Login";
+})
+
+signupBtn.addEventListener("click",()=>{
+    moveBtn.classList.remove("rightBtn");
+    signup.classList.add("signupform");
+    login.classList.remove("loginform");
+    moveBtn.innerHTML = "signup";
+})
